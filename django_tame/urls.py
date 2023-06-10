11fb1a5b30ba9b81a15f212b441080d06af8bbe7
Tamirucode <>
2023-06-10 05:13:58 +0000

    update initial setting

--- django_tame/urls.py
+++ django_tame/urls.py
@@ -15,9 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import get_todo_list
+from tame import views
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
-    path('get_todo_list/', get_todo_list, name='get_todo_list'),
+    path('', views.home, name="home"),
+    path('findbus', views.findtable, name="findbus"),
+    path('bookings', views.bookings, name="bookings"),
+    path('cancellings', views.cancellings, name="cancellings"),
+    path('mybookings', views.mybookings, name="mybookings"),
+    path('signup', views.signup, name="signup"),
+    path('signin', views.signin, name="signin"),
+    path('success', views.success, name="success"),
+    path('signout', views.signout, name="signout"),
+
 ]
---
+++ my_djanjo_p/my_djanjo_p/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from tam import views
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('findbus', views.findtable, name="findbus"),
+    path('bookings', views.bookings, name="bookings"),
+    path('cancellings', views.cancellings, name="cancellings"),
+    path('mybookings', views.mybookings, name="mybookings"),
+    path('signup', views.signup, name="signup"),
+    path('signin', views.signin, name="signin"),
+    path('success', views.success, name="success"),
+    path('signout', views.signout, name="signout"),
+
+]
